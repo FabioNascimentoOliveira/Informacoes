588eb3d424b37134f0c138ed3b4b8d6af28186e6
fabio.binholiver@gmail.com <fabio.binholiliver@gmail.com>
2022-06-25 18:56:26 -0300

    Informacoes pessoais

---
+++ Nome.js
@@ -0,0 +1,11 @@
+$("#p1").css("color", "red");
+$("#p2").css("color", "blue");
+$("#p2").css("font-size", "30px");
+$("#p3").css("color", "green");
+$("#p3").css("font-size", "40px",);
+//animar
+
+
+
+
+
